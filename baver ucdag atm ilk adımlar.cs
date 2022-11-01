ee0f7b11838726ab3ff58670f8fe08c2112e4103
samet ucdag <111017028+Sametucdag@users.noreply.github.com>
2022-11-01 17:13:19 +0300

    Add files via upload

---
+++ baver ucdag atm ilk adımlar.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+//atm yapımı 10.10.22
+//c# ilk adım 
+//para cekme a
+//para yatırma
+//bakiye görüntüleme
+//*basıp çıkış yapma
+//Hangi işlemi yapmak istiyeceği
+
+int bakiye = 1000;
+
+Console.WriteLine("atm'ye hoşgeldiniz");
+Console.WriteLine("Yapmak istediğiniz işlemi seçiniz");
+string seçim = Console.ReadLine();
+
+    if (seçim== "1") ;
+{
+Console.WriteLine("Şuanki bakiye:" + bakiye);
+    Console.ReadLine();
+}
+   if (seçim == "2") ;
+{
+    Console.WriteLine("çekmek istediğiniz tutarı belirtiniz");
+    int çekilecek_tutar = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("kalan tutar :"+(bakiye - çekilecek_tutar));
+    Console.ReadLine();
+}
+if (seçim == "3") ;
+{
+    Console.WriteLine("yatırmak istediğiniz tutarı belirtiniz");
+    int yatırılıcak_tutar = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("yatırılan tutar :" + (bakiye + yatırılıcak_tutar));
+    Console.ReadLine();
+}
+if (seçim == "*") ;
+{
+    Console.WriteLine("atm den çıkış yapılıyor");
+    Console.WriteLine("atm den çıkış yapıldı başarılı günler dilerim ...");
+    Console.ReadLine();
+        
+}
+ 
+
+
+
+
+
+
+
---
+++ baver ucdag login uygulaması.cs
@@ -0,0 +1,43 @@
+﻿//login (giriş) uygulaması
+//baver ucdag login uygulaması ilk adımlar 
+
+int hak_sayisi = 3;
+while (true)
+{
+    Console.WriteLine("kullanıcı adını giriniz");
+    string kullaniciadi = Console.ReadLine();
+
+    Console.WriteLine("şifrenizi giriniz");
+    string sifre = Console.ReadLine();
+
+
+    if (kullaniciadi == "baver" && sifre == "baver1234")
+    {
+        Console.WriteLine("tebrikler başarılı giriş yaptınız");
+        Console.ReadLine();
+        break;
+    }
+    else
+    {
+        Console.WriteLine("kullanıcı adınız veya şifreniz yanlıştır !");
+        if (hak_sayisi > 0)
+        {
+            hak_sayisi -= 1;
+
+        }
+
+        if (hak_sayisi == 0) ;
+        {
+            Console.WriteLine("lütfen tekrar deneyiniz");
+        }
+    }
+
+}
+
+Console.ReadLine();
+
+            
+            
+
+            
+        
